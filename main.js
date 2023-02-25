d4d43fcbd45391cfc6c49b4f9e0b23a3ca153581
enzomaestri <enzosm@gmail.com>
2023-02-25 13:05:21 -0300

    projeto_final

--- main.js
+++ main.js
@@ -1,20 +1,61 @@
-const sum = document.querySelector("#sum");
-const sub = document.querySelector("#sub");
-const countArm = document.querySelector("#count-arm");
 
-const control = document.querySelectorAll(".controle-ajuste");
+const control = document.querySelectorAll("[data-controle]");
+const statistics = document.querySelectorAll("[data-statistic]");
+
+const parts = {
+    "arms": {
+        "strength": 29,
+        "power": 35,
+        "energy": -21,
+        "speed": -5
+    },
+
+    "armor": {
+        "strength": 41,
+        "power": 20,
+        "energy": 0,
+        "speed": -20
+    },
+    "cores":{
+        "strength": 0,
+        "power": 7,
+        "energy": 48,
+        "speed": -24
+    },
+    "legs":{
+        "strength": 27,
+        "power": 21,
+        "energy": -32,
+        "speed": 42
+    },
+    "rockets":{
+        "strength": 0,
+        "power": 28,
+        "energy": 0,
+        "speed": -2
+    }
+}
 
 control.forEach( (element) => {
     element.addEventListener("click", (event) => {
-        changeData(event.target.textContent);
+        changeData(event.target.dataset.controle, event.target.parentNode);
+        updateStatistics(event.target.dataset.part);
     })
 })
 
 
-function changeData(operation, bodyPart) {
+function changeData(operation, control) {
+    const part = control.querySelector("[data-contador]");
+
     if (operation === "-") {
-        countArm.value = parseInt(countArm.value) - 1;
+        part.value = parseInt(part.value) - 1;
     } else {
-        countArm.value = parseInt(countArm.value) + 1;
+        part.value = parseInt(part.value) + 1;
     }
+}
+
+function updateStatistics(part) {
+    statistics.forEach( (element) => {
+        element.textContent = parseInt(element.textContent) + parts[part][element.dataset.statistic];
+    })
 }
